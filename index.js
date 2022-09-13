9a344eb1ba719234dd9449297d3caec6630044f1
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-09-13 14:31:16 +0000

    Style desktop version and complete dynamic section for the featured speakers

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const lastFourSpeakers = [
     id: 'speaker3',
     img: 'Images/speaker2.jpg',
     imgCheckers: 'Images/Checkers-img.png',
-    class: 'speakers3',
+    class: 'speakers32',
     name: 'Jay Ekow Melfah',
     credentials: 'Director of Moode Art Enterprise',
     Interest: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga culpa vitae sed est beatae ex, possimus odio.     Incidunt, excepturi quo.',
@@ -105,7 +105,7 @@ const lastFourSpeakers = [
     id: 'speaker4',
     img: 'Images/speaker3.jpg',
     imgCheckers: 'Images/Checkers-img.png',
-    class: 'speakers4',
+    class: 'speakers42',
     name: 'Laura Lordanna',
     credentials: 'Artist at Moode Art Enterprise',
     Interest: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga culpa vitae sed est beatae ex, possimus odio.     Incidunt, excepturi quo.',
@@ -114,7 +114,7 @@ const lastFourSpeakers = [
     id: 'speaker5',
     img: 'Images/speaker5.jpg',
     imgCheckers: 'Images/Checkers-img.png',
-    class: 'speakers5',
+    class: 'speakers52',
     name: 'Carimae Lucy Andelle',
     credentials: 'Photographer at Moode Art Enterprise',
     Interest: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga culpa vitae sed est beatae ex, possimus odio.     Incidunt, excepturi quo.',
@@ -123,13 +123,15 @@ const lastFourSpeakers = [
     id: 'speaker6',
     img: 'Images/speaker6.jpg',
     imgCheckers: 'Images/Checkers-img.png',
-    class: 'speakers6',
+    class: 'speakers62',
     name: 'Aurora Altaria',
     credentials: 'Photographer at Moode Art Enterprise',
     Interest: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga culpa vitae sed est beatae ex, possimus odio.     Incidunt, excepturi quo.',
   },
 ];
 const speakersCont = document.querySelector('.speakers-container');
+const speakersCont2 = document.querySelector('.speakers-container2');
+const speakersCont3 = document.querySelector('.speakers-container3');
 firstTwoSpeakers.forEach((obj) => {
   const div1 = document.createElement('div');
   div1.classList.add(obj.class);
@@ -159,3 +161,79 @@ firstTwoSpeakers.forEach((obj) => {
   div1.append(div2, div3);
   speakersCont.appendChild(div1);
 });
+
+const seeMore = document.querySelector('.speakers-seemore');
+seeMore.addEventListener('click', () => {
+  if (seeMore.innerHTML === 'MORE <i id="button-arrow" class="fa-solid fa-chevron-down"></i>') {
+    document.querySelector('.speakers-container2').style.gridTemplateRows = '1fr 1fr 1fr 1fr 1fr 1fr';
+    lastFourSpeakers.forEach((obj) => {
+      const div1 = document.createElement('div');
+      div1.classList.add(obj.class);
+      const div2 = document.createElement('div');
+      div2.classList.add('speaker-img');
+      const img1 = document.createElement('img');
+      img1.classList.add('checkers-img');
+      img1.src = obj.imgCheckers;
+      const img2 = document.createElement('img');
+      img2.classList.add('imgspk');
+      img2.src = obj.img;
+      const div3 = document.createElement('div');
+      div3.classList.add('speaker-details');
+      const h3 = document.createElement('h3');
+      h3.classList.add('speaker-name');
+      h3.textContent = obj.name;
+      const h4 = document.createElement('h4');
+      h4.classList.add('speaker-credentials');
+      h4.textContent = obj.credentials;
+      const span = document.createElement('span');
+      span.classList.add('small-line', 'cal');
+      const par = document.createElement('p');
+      par.classList.add('speaker-interest');
+      par.textContent = obj.Interest;
+      div3.append(h3, h4, span, par);
+      div2.append(img1, img2);
+      div1.append(div2, div3);
+      speakersCont2.appendChild(div1);
+      document.querySelector('.footer-section').style.display = 'block';
+      document.querySelector('.speakers-container2').style.display = 'grid';
+      seeMore.innerHTML = 'LESS <i id="button-arrow" class="fa-solid fa-angle-up"></i>';
+    });
+  } else {
+    document.querySelector('.footer-section').style.display = 'none';
+    document.querySelector('.speakers-container2').style.display = 'none';
+    seeMore.innerHTML = 'MORE <i id="button-arrow" class="fa-solid fa-chevron-down"></i>';
+  }
+});
+
+const query = window.matchMedia('(min-width: 768px)');
+if (query.matches) {
+  featuredSpeakers.forEach((obj) => {
+    const div1 = document.createElement('div');
+    div1.classList.add(obj.class);
+    const div2 = document.createElement('div');
+    div2.classList.add('speaker-img');
+    const img1 = document.createElement('img');
+    img1.classList.add('checkers-img');
+    img1.src = obj.imgCheckers;
+    const img2 = document.createElement('img');
+    img2.classList.add('imgspk');
+    img2.src = obj.img;
+    const div3 = document.createElement('div');
+    div3.classList.add('speaker-details');
+    const h3 = document.createElement('h3');
+    h3.classList.add('speaker-name');
+    h3.textContent = obj.name;
+    const h4 = document.createElement('h4');
+    h4.classList.add('speaker-credentials');
+    h4.textContent = obj.credentials;
+    const span = document.createElement('span');
+    span.classList.add('small-line', 'cal');
+    const par = document.createElement('p');
+    par.classList.add('speaker-interest');
+    par.textContent = obj.Interest;
+    div3.append(h3, h4, span, par);
+    div2.append(img1, img2);
+    div1.append(div2, div3);
+    speakersCont3.appendChild(div1);
+  });
+}
